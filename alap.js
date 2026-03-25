00400d467f3a00bbf66040e80288a7b9a98a6f58
Sajat Nev <email@pelda.hu>
2026-03-25 08:21:22 +0100

    Képek

---
+++ alap.js
@@ -0,0 +1,8 @@
+sz=""
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 4; j++) {
+        sz+=`<img src="img/a0.jpg" class="kvizkep">`
+    }
+    sz+="<br>"
+}
+document.getElementById("kepek").innerHTML=sz
