73c759f79a0e54f24c613e90c23a3f4dc5b37eb3
Sajat Nev <email@pelda.hu>
2026-03-30 08:08:11 +0200

    Képek hozzáadása

---
+++ adatok.js
@@ -0,0 +1,74 @@
+const nevekTomb = [
+    {
+        "megoldas": "Mátyás király",
+        "tipp1": "Lajos király",
+        "tipp2": "IV. Béla",
+        "tipp3": "II. András"
+    },
+    {
+        "megoldas": "Mária Terézia",
+        "tipp1": "Sissi királyné",
+        "tipp2": "Viktória királynő",
+        "tipp3": "I. Erzsébet"
+    },
+    {
+        "megoldas": "Széchenyi István",
+        "tipp1": "Kossuth Lajos",
+        "tipp2": "Deák Ferenc",
+        "tipp3": "Wesselényi Miklós"
+    },
+    {
+        "megoldas": "Kossuth Lajos",
+        "tipp1": "Széchenyi István",
+        "tipp2": "Bem József",
+        "tipp3": "Petőfi Sándor"
+    },
+    {
+        "megoldas": "Churchill",
+        "tipp1": "Franklin D. Roosevelt",
+        "tipp2": "Joseph Stalin",
+        "tipp3": "Dwight D. Eisenhower"
+    },
+    {
+        "megoldas": "Abraham Lincoln",
+        "tipp1": "George Washington",
+        "tipp2": "Thomas Jefferson",
+        "tipp3": "Theodore Roosevelt"
+    },
+    {
+        "megoldas": "Napóleon",
+        "tipp1": "XIV. Lajos",
+        "tipp2": "III. Napóleon",
+        "tipp3": "Robespierre"
+    },
+    {
+        "megoldas": "Sissi királyné",
+        "tipp1": "Mária Terézia",
+        "tipp2": "Zita királyné",
+        "tipp3": "Stefánia hercegné"
+    },
+    {
+        "megoldas": "John F. Kennedy",
+        "tipp1": "Barack Obama",
+        "tipp2": "Nelson Mandela",
+        "tipp3": "Abraham Lincoln"
+    },
+    {
+        "megoldas": "Deák Ferenc",
+        "tipp1": "Kossuth Lajos",
+        "tipp2": "Széchenyi István",
+        "tipp3": "Eötvös József"
+    },
+    {
+        "megoldas": "Nelson Mandela",
+        "tipp1": "Desmond Tutu",
+        "tipp2": "Martin Luther King Jr.",
+        "tipp3": "Barack Obama"
+    },
+    {
+        "megoldas": "Gandhi",
+        "tipp1": "Nelson Mandela",
+        "tipp2": "Jawaharlal Nehru",
+        "tipp3": "Dalai Láma"
+    }
+];
--- alap.js
+++ alap.js
@@ -1,8 +1,40 @@
 sz=""
+sorszam=0
 for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 4; j++) {
-        sz+=`<img src="img/a0.jpg" class="kvizkep">`
+        sz+=`<img src="img/a${sorszam}.jpg" class="kvizkep" onclick="kerdes(${sorszam})">`
+        sorszam++
     }
     sz+="<br>"
 }
 document.getElementById("kepek").innerHTML=sz
+
+function kerdes(szam){
+    sz=""
+    sz+=`<img src="img/a${szam}.jpg">`
+    document.getElementById("nagyKep").innerHTML=sz
+
+    szavakTomb=[]
+    szavakTomb.push(nevekTomb[szam].megoldas)
+    szavakTomb.push(nevekTomb[szam].tipp1)
+    szavakTomb.push(nevekTomb[szam].tipp2)
+    szavakTomb.push(nevekTomb[szam].tipp3)
+
+    for (let i = 0; i < 100; i++) {
+        r1=Math.floor(Math.random()*szavakTomb.length)
+        r2=Math.floor(Math.random()*szavakTomb.length)
+        temp=szavakTomb[r1]
+        szavakTomb[r1]=szavakTomb[r2]
+        szavakTomb[r2]=temp
+    }
+
+    gombok=""
+    for (let i = 0; i < 4; i++) {
+        gombok+=`<button onclick="ertekel('${szavakTomb[i]}')">${szavakTomb[i]}</button>`
+    }
+    document.getElementById("gombok").innerHTML=gombok
+}
+
+function ertekel(szemely){
+    alert(szemely)
+}
